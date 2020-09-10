86d36096c0e666ddd983a6432a06db117e8357b3
Miku Sano <miku39.sano@gmail.com>
2020-09-11 08:37:48 +0900

    feat(book): add seed to books table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create!(
+    isbn: '1617292397',
+    image_url: 'https://books.google.com/books/content?id=UXNaAQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+    title: 'Soft Skills',
+    author: 'John Z. Sonmez',
+    date: '2019-12-17',
+    note: '面白くてタメになった'
+)
