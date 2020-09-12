437c41e80a78482d9941760eeaf121d7aa904943
Miku Sano <miku39.sano@gmail.com>
2020-09-12 11:11:40 +0900

    feat(book): get book data from DB

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,38 +1,19 @@
 class BooksController < ApplicationController
     # 本のリストを表示する
     def index
-        @books = [
-            {
-                id: 1,
-                isbn: '1617292397',
-                image_url: 'https://books.google.com/books/content?id=UXNaAQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
-                title: 'Soft Skills',
-                author: 'John Z. Sonmez',
-                date: '2019-12-17'
-            },
-            {
-                id: 2,
-                isbn: '1617292397',
-                image_url: 'https://books.google.com/books/content?id=UXNaAQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
-                title: 'Soft Skills',
-                author: 'John Z. Sonmez',
-                date: '2019-12-17'
-            }
-        ]
+        @books = Book.all
         render 'index', formats: :json, handlers: 'jbuilder'
     end
 
     # 本の詳細を表示する
     def show
-        @book = {
-                id: params[:id],
-                isbn: '1617292397',
-                image_url: 'https://books.google.com/books/content?id=UXNaAQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
-                title: 'Soft Skills',
-                author: 'John Z. Sonmez',
-                date: '2019-12-17',
-                note: '面白くてタメになった'
-            }
+        @book = Book.find_by_id(params[:id])
+        # 本がない場合
+        if @book.nil?
+            render status: 404, json: { status: 404, message: "id:#{params[:id]} Not Found" }
+            return
+        end
+
         render 'show', formats: :json, handlers: 'jbuilder'
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
 Book.create!(
-    isbn: '1617292397',
-    image_url: 'https://books.google.com/books/content?id=UXNaAQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
-    title: 'Soft Skills',
-    author: 'John Z. Sonmez',
-    date: '2019-12-17',
-    note: '面白くてタメになった'
-)
+    [
+      {
+        isbn: '1617292397',
+        image_url: 'https://books.google.com/books/content?id=UXNaAQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+        title: 'Soft Skills',
+        author: 'John Z. Sonmez',
+        date: '2019-12-17',
+        note: 'タメになった'
+      },
+      {
+        isbn: '4801400604',
+        image_url: 'https://books.google.com/books/content?id=uKsyvwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+        title: 'お金のこと何もわからないままフリーランスになっちゃいましたが税金で損しない方法を教えてください!',
+        author: '大河内薫 / 若林杏樹',
+        date: '2020-08-02',
+        note: 'スラスラ読めた'
+      }
+    ]
+  )
