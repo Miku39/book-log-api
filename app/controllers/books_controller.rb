5ff6041b4bd10fe85e96478d37dfb1ae14d274c3
Miku Sano <miku39.sano@gmail.com>
2020-11-06 08:23:53 +0900

    test: post response to slack

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,9 +28,14 @@ class BooksController < ApplicationController
     # Slack から本を登録する
     def slack
         # TODO delete
-        p "slack method!!!"
-        @book = Book.create_with_isbn(params[:text])
+        p "slack method!!!" 
+        p params
+
         # 送られてきたISBNを使用して本の情報を取得する
+        @book = Book.create_with_isbn(params[:text])
+
+        p @book
+        
         url = URI.parse(params[:response_url])
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
@@ -43,21 +48,23 @@ class BooksController < ApplicationController
         }
 
         p 'print slackResponse start'
-        p slackResponse
+        p slackResponse.to_json
+        p url.path
+        p headers
         p 'print slackResponse end'
 
         # TODO slack body 調べる
-        response = http.post(url.path, slackResponse.to_json, headers)
+        # response = http.post(url.path, slackResponse.to_json, headers)
 
-        p 'response start'
-        p response
-        p 'response end'
+        # p 'response start'
+        # p response
+        # p 'response end'
 
-        responseJson = response.read_body
+        # responseJson = response.read_body
        
-        p 'hash start'
-        p JSON.parse(responseJson)
-        p 'hash end'
+        # p 'hash start'
+        # p JSON.parse(responseJson)
+        # p 'hash end'
 
         render 'show', formats: :json, handlers: 'jbuilder'
     end
